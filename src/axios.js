c550335fee620d9b9b47d472953cfee7ec8b0fd9
DisaniaS <108000102+DisaniaS@users.noreply.github.com>
2024-05-24 22:40:42 +0300

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: https://disanias-diploma-back-52bc.twc1.net
+    baseURL: "https://disanias-diploma-back-52bc.twc1.net"
 })
 
 instance.interceptors.request.use((config) => {
