9f0fe9701b1d1b4b221b0a4ad996758a5ded72a9
DisaniaS <serzh.30303@mail.ru>
2024-05-27 22:05:12 +0300

    adaptation on mobile

--- src/components/pages/ProjectMain/Graph/Graph.jsx
+++ src/components/pages/ProjectMain/Graph/Graph.jsx
@@ -1,54 +1,220 @@
+// import { Typography } from '@mui/material';
+// import react, { useState, useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import Graph from 'react-vis-network-graph';
+// import styles from "./GraphPage.module.css"
+// import { fetchTasks } from '../../../../redux/slices/tasks';
+// import { createEdge } from '../../../../redux/slices/edge';
+// import { deleteEdge } from '../../../../redux/slices/edge';
+// import { fetchEdges } from '../../../../redux/slices/edges';
+
+
+
+// export default function GraphPage({show}) {
+//   const dispatch = useDispatch()
+//   const { tasks } = useSelector(state => state.tasks)
+//   const isTasksLoaded = tasks.status === 'loaded'
+
+//   const { edgesss } = useSelector(state => state.edgesss)
+//   const isEdgesLoaded = tasks.status === 'loaded'
+
+//   const [physic, setPhysic] = useState(true);
+
+//   const [nodes, setNodes] = useState(null);
+//   const [edges, setEdges] = useState(null);
+//   const [selectedNode, setSelectedNode] = useState(null)
+//   const [secondSelectedNode, setSecondSelectedNode] = useState(null);
+//   const [selectedEdge, setSelectedEdge] = useState(null);
+
+//   useEffect(() => {
+//     dispatch(fetchTasks({projectid: window.localStorage.getItem("projectid")}))
+//     dispatch(fetchEdges({projectid: window.localStorage.getItem("projectid")}))
+//     const jsonNodes = JSON.parse(JSON.stringify({nodes: tasks.items.map(obj => ({
+//        id: obj.taskid, 
+//        label: obj.taskname, 
+//        color: obj.iscomplete ? "lightgreen" : 'lightblue'}
+//        ))})).nodes;
+//     setNodes(jsonNodes)
+
+//     const jsonEdges = JSON.parse(JSON.stringify({edges: edgesss.items.map(obj => ({
+//       from: obj.edgefrom, 
+//       to: obj.edgeto
+//     }))})).edges;
+//     setEdges(jsonEdges);
+//   }, [show])  
+//   useEffect(() => {
+//   }, [selectedNode, secondSelectedNode, selectedEdge, edges]);
+
+//   const graph = {
+//     nodes: nodes,
+//     edges: edges
+//   }
+
+//   const options = {
+//     layout: {
+//       hierarchical: false
+//     },
+//     edges: {
+//       color: "#000000",
+//       length: 250
+//     },
+//     nodes: {
+//       shape: 'box',
+//       margin: {
+//         top: 10,
+//         bottom: 10,
+//         left: 20,
+//         right: 20
+//       }
+//     },
+//     physics: physic
+//   };
+
+//   const handleAddEdge = () => {
+//     if (selectedNode !== null && secondSelectedNode !== null) {
+//       setEdges([...edges, {from: selectedNode+"", to: secondSelectedNode+""}]);
+//       dispatch(createEdge({projectid: window.localStorage.getItem("projectid"), taskid: selectedNode, edgefrom: selectedNode, edgeto: secondSelectedNode}))
+//       setSelectedNode(null);
+//       setSecondSelectedNode(null);
+//       setSelectedEdge(null)
+//     }
+//   }
+  
+//   const handleDeleteEdge = () => {
+//     if (selectedEdge !== null) {
+//       const newEdges = edges.filter(edge => !(edge.from === selectedEdge.from+"" && edge.to ===  selectedEdge.to+""));
+//       setEdges(newEdges);
+//       dispatch(deleteEdge({edgefrom: selectedEdge.from+"", edgeto: selectedEdge.to+""}))
+//       setSelectedNode(null);
+//       setSecondSelectedNode(null);
+//       setSelectedEdge(null)
+//     }
+//   }
+
+//   const handleAddEdgeCancel = () => {
+//     setSelectedNode(null)
+//     setSecondSelectedNode(null)
+//     setSelectedEdge(null)
+//   }
+
+//   const selectNode = (event) => {
+//     var node = event.nodes[0]
+    
+//     if (selectedNode === null) {
+//       setSelectedNode(node)
+//       console.log(node + "   " + selectedNode)
+//     } else if(secondSelectedNode === null){
+//       setSecondSelectedNode(node)
+//       console.log(node + "   " + selectedNode)
+//     }
+//     console.log(selectedNode + "  to  " + secondSelectedNode)
+//   }
+
+//   const selectEdge = (event) => {
+//     var selectedEdgeId = event.edges[0]
+//     const edgeToDelete = edges.find(edge => edge.id === selectedEdgeId);
+//     setSelectedEdge(edgeToDelete)
+//   }
+//   const events = {
+//     selectNode,
+//     selectEdge
+//   };
+  
+//     if(show) { 
+//         return(
+//               <div className={styles.content}>
+//                   <Graph
+//                     className={styles.graph}
+//                     graph={graph} 
+//                     options={options} 
+//                     events = {events}
+//                   />
+//                   <div className={styles.controlPanel}>
+//                     {selectedNode!=null 
+//                     ? (
+//                       <p>1 узел выбран</p>
+//                     )
+//                     : (
+//                       <p></p>
+//                     )}
+//                     {secondSelectedNode!=null 
+//                     ? (
+//                       <p>2 узел выбран</p>
+//                     )
+//                     : (
+//                       <p></p>
+//                     )}
+//                     {selectedEdge!=null 
+//                     ? (
+//                       <p>Ребро выбрано</p>
+//                     )
+//                     : (
+//                       <p></p>
+//                     )}
+//                     <button className={styles.button} onClick={handleAddEdge}>Добавить ребро</button>
+//                     <button className={styles.button} onClick={handleDeleteEdge}>Удалить ребро</button>
+//                     <button className={styles.button} onClick={handleAddEdgeCancel}>Отменить</button>
+//                     <button className={styles.button} onClick={()=>{setPhysic(value => !value)}}>Физика</button>
+//                     {physic 
+//                     ? (
+//                       <p>Включена</p>
+//                     )
+//                     : (
+//                       <p>выключена</p>
+//                     )}
+//                   </div>
+//               </div>
+//         ) 
+//     }
+// }
 import { Typography } from '@mui/material';
-import react, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Graph from 'react-vis-network-graph';
-import styles from "./GraphPage.module.css"
+import styles from "./GraphPage.module.css";
 import { fetchTasks } from '../../../../redux/slices/tasks';
 import { createEdge } from '../../../../redux/slices/edge';
 import { deleteEdge } from '../../../../redux/slices/edge';
 import { fetchEdges } from '../../../../redux/slices/edges';
 
-
-
-export default function GraphPage({show}) {
-  const dispatch = useDispatch()
-  const { tasks } = useSelector(state => state.tasks)
-  const isTasksLoaded = tasks.status === 'loaded'
-
-  const { edgesss } = useSelector(state => state.edgesss)
-  const isEdgesLoaded = tasks.status === 'loaded'
+export default function GraphPage({ show }) {
+  const dispatch = useDispatch();
+  const { tasks } = useSelector(state => state.tasks);
+  const { edgesss } = useSelector(state => state.edgesss);
 
   const [physic, setPhysic] = useState(true);
-
   const [nodes, setNodes] = useState(null);
   const [edges, setEdges] = useState(null);
-  const [selectedNode, setSelectedNode] = useState(null)
+  const [selectedNode, setSelectedNode] = useState(null);
   const [secondSelectedNode, setSecondSelectedNode] = useState(null);
   const [selectedEdge, setSelectedEdge] = useState(null);
 
   useEffect(() => {
-    dispatch(fetchTasks({projectid: window.localStorage.getItem("projectid")}))
-    dispatch(fetchEdges({projectid: window.localStorage.getItem("projectid")}))
-    const jsonNodes = JSON.parse(JSON.stringify({nodes: tasks.items.map(obj => ({
-       id: obj.taskid, 
-       label: obj.taskname, 
-       color: obj.iscomplete ? "lightgreen" : 'lightblue'}
-       ))})).nodes;
-    setNodes(jsonNodes)
+    dispatch(fetchTasks({ projectid: window.localStorage.getItem("projectid") }));
+    dispatch(fetchEdges({ projectid: window.localStorage.getItem("projectid") }));
+    
+    const jsonNodes = JSON.parse(JSON.stringify({
+      nodes: tasks.items.map(obj => ({
+        id: obj.taskid,
+        label: obj.taskname,
+        color: obj.iscomplete ? "lightgreen" : 'lightblue'
+      }))
+    })).nodes;
+    setNodes(jsonNodes);
 
-    const jsonEdges = JSON.parse(JSON.stringify({edges: edgesss.items.map(obj => ({
-      from: obj.edgefrom, 
-      to: obj.edgeto
-    }))})).edges;
+    const jsonEdges = JSON.parse(JSON.stringify({
+      edges: edgesss.items.map(obj => ({
+        from: obj.edgefrom,
+        to: obj.edgeto
+      }))
+    })).edges;
     setEdges(jsonEdges);
-  }, [show])  
-  useEffect(() => {
-  }, [selectedNode, secondSelectedNode, selectedEdge, edges]);
+  }, [show]);
 
   const graph = {
     nodes: nodes,
     edges: edges
-  }
+  };
 
   const options = {
     layout: {
@@ -72,98 +238,80 @@ export default function GraphPage({show}) {
 
   const handleAddEdge = () => {
     if (selectedNode !== null && secondSelectedNode !== null) {
-      setEdges([...edges, {from: selectedNode+"", to: secondSelectedNode+""}]);
-      dispatch(createEdge({projectid: window.localStorage.getItem("projectid"), taskid: selectedNode, edgefrom: selectedNode, edgeto: secondSelectedNode}))
+      setEdges([...edges, { from: selectedNode, to: secondSelectedNode }]);
+      dispatch(createEdge({
+        projectid: window.localStorage.getItem("projectid"),
+        taskid: selectedNode,
+        edgefrom: selectedNode,
+        edgeto: secondSelectedNode
+      }));
       setSelectedNode(null);
       setSecondSelectedNode(null);
-      setSelectedEdge(null)
+      setSelectedEdge(null);
     }
-  }
-  
+  };
+
   const handleDeleteEdge = () => {
     if (selectedEdge !== null) {
-      const newEdges = edges.filter(edge => !(edge.from === selectedEdge.from+"" && edge.to ===  selectedEdge.to+""));
+      const newEdges = edges.filter(edge => !(edge.from === selectedEdge.from && edge.to === selectedEdge.to));
       setEdges(newEdges);
-      dispatch(deleteEdge({edgefrom: selectedEdge.from+"", edgeto: selectedEdge.to+""}))
+      dispatch(deleteEdge({ edgefrom: selectedEdge.from, edgeto: selectedEdge.to }));
       setSelectedNode(null);
       setSecondSelectedNode(null);
-      setSelectedEdge(null)
+      setSelectedEdge(null);
     }
-  }
+  };
 
   const handleAddEdgeCancel = () => {
-    setSelectedNode(null)
-    setSecondSelectedNode(null)
-    setSelectedEdge(null)
-  }
+    setSelectedNode(null);
+    setSecondSelectedNode(null);
+    setSelectedEdge(null);
+  };
 
   const selectNode = (event) => {
-    var node = event.nodes[0]
-    
+    const node = event.nodes[0];
     if (selectedNode === null) {
-      setSelectedNode(node)
-      console.log(node + "   " + selectedNode)
-    } else if(secondSelectedNode === null){
-      setSecondSelectedNode(node)
-      console.log(node + "   " + selectedNode)
+      setSelectedNode(node);
+    } else if (secondSelectedNode === null) {
+      setSecondSelectedNode(node);
     }
-    console.log(selectedNode + "  to  " + secondSelectedNode)
-  }
+  };
 
   const selectEdge = (event) => {
-    var selectedEdgeId = event.edges[0]
+    const selectedEdgeId = event.edges[0];
     const edgeToDelete = edges.find(edge => edge.id === selectedEdgeId);
-    setSelectedEdge(edgeToDelete)
-  }
+    setSelectedEdge(edgeToDelete);
+  };
+
   const events = {
     selectNode,
     selectEdge
   };
-  
-    if(show) { 
-        return(
-              <div className={styles.content}>
-                  <Graph
-                    className={styles.graph}
-                    graph={graph} 
-                    options={options} 
-                    events = {events}
-                  />
-                  <div className={styles.controlPanel}>
-                    {selectedNode!=null 
-                    ? (
-                      <p>1 узел выбран</p>
-                    )
-                    : (
-                      <p></p>
-                    )}
-                    {secondSelectedNode!=null 
-                    ? (
-                      <p>2 узел выбран</p>
-                    )
-                    : (
-                      <p></p>
-                    )}
-                    {selectedEdge!=null 
-                    ? (
-                      <p>Ребро выбрано</p>
-                    )
-                    : (
-                      <p></p>
-                    )}
-                    <button className={styles.button} onClick={handleAddEdge}>Добавить ребро</button>
-                    <button className={styles.button} onClick={handleDeleteEdge}>Удалить ребро</button>
-                    <button className={styles.button} onClick={handleAddEdgeCancel}>Отменить</button>
-                    <button className={styles.button} onClick={()=>{setPhysic(value => !value)}}>Физика</button>
-                    {physic 
-                    ? (
-                      <p>Включена</p>
-                    )
-                    : (
-                      <p>выключена</p>
-                    )}
-                  </div>
-              </div>
-        ) 
-    }
+
+  if (show) {
+    return (
+      <div className={styles.content}>
+        <Graph
+          className={styles.graph}
+          graph={graph}
+          options={options}
+          events={events}
+        />
+        <div className={styles.controlPanel}>
+          <div className={styles.buttons}>
+            <button className={styles.button} onClick={handleAddEdge}>Добавить</button>
+            <button className={styles.button} onClick={handleDeleteEdge}>Удалить</button>
+            <button className={styles.button} onClick={handleAddEdgeCancel}>Отменить</button>
+            <button className={styles.button} onClick={() => setPhysic(!physic)}>Физика: {physic ? 'ON' : 'OFF'}</button>
+          </div>
+          <div className={styles.texts}>
+            {selectedNode && <p>1 узел выбран</p>}
+            {secondSelectedNode && <p>2 узел выбран</p>}
+            {selectedEdge && <p>Ребро выбрано</p>}
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return null;
 }
--- src/components/pages/ProjectMain/Table/TableTasks.jsx
+++ src/components/pages/ProjectMain/Table/TableTasks.jsx
@@ -1,59 +1,118 @@
-import react, { useState, useEffect } from 'react';
+// import react, { useState, useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { fetchTasks } from '../../../../redux/slices/tasks';
+// import Typography from '@mui/material/Typography';
+// import styles from './TableTasks.module.css';
+// import { Task } from '../../../elements/tasks/task';
+// import CreateTaskForm from '../../../elements/taskForm/CreateTaskForm'
+
+// export default function TableTasks({show}) {
+//     const dispatch = useDispatch()
+//     const { tasks } = useSelector(state => state.tasks)
+//     const isTasksLoading = tasks.status === 'loading'
+    
+//     const [showCreateTaskForm, setShowCreateTaskForm] = useState(false)
+//     const showCTF = () => {setShowCreateTaskForm(true)}
+//     const hideCTF = () => {setShowCreateTaskForm(false)}
+
+//     useEffect(() => {
+//         dispatch(fetchTasks({projectid: window.localStorage.getItem("projectid")}))
+//     }, [showCreateTaskForm, show])
+
+
+//     if(show) { 
+//         return(
+//             <>
+//                 <div className={styles.content}>
+//                     <CreateTaskForm show={showCreateTaskForm} onClose={hideCTF}/>
+//                     <div className={styles.tasksCurrent}>
+//                     <div className={styles.tasksHeader}>
+//                         <Typography variant='h5' className={styles.tiitle}>Текущие</Typography>
+//                         <button onClick={showCTF} className={styles.button}>Добавить</button>
+//                     </div>
+//                         {tasks.items.map((obj, index) => 
+//                         isTasksLoading || obj.iscomplete === true ? (
+//                             <>
+//                             </>
+//                         ) : (
+//                             <Task
+//                                 key={index}
+//                                 taskid={obj.taskid}
+//                                 taskname={obj.taskname}
+//                                 executor={obj.executor}
+//                                 iscomplet={obj.iscomplete}
+//                             />
+//                         ))}
+//                     </div>
+                
+//                     <div className={styles.tasksCompleted}>
+//                         <div className={styles.tasksHeader}>
+//                             <Typography variant='h5' className={styles.tiitle}>Выполненные</Typography>
+//                         </div>
+//                             {tasks.items.map((obj, index) => 
+//                             isTasksLoading || obj.iscomplete === false ? (
+//                                 <>
+//                                 </>
+//                             ) : (
+//                                 <Task
+//                                     key={index}
+//                                     taskid={obj.taskid}
+//                                     taskname={obj.taskname}
+//                                     executor={obj.executor}
+//                                     iscomplet={obj.iscomplete}
+//                                 />
+//                             ))}
+//                     </div>
+//                 </div>
+//             </>
+//         ) 
+//     }
+// }
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTasks } from '../../../../redux/slices/tasks';
 import Typography from '@mui/material/Typography';
+import Slider from 'react-slick';
 import styles from './TableTasks.module.css';
 import { Task } from '../../../elements/tasks/task';
-import CreateTaskForm from '../../../elements/taskForm/CreateTaskForm'
+import CreateTaskForm from '../../../elements/taskForm/CreateTaskForm';
 
-export default function TableTasks({show}) {
-    const dispatch = useDispatch()
-    const { tasks } = useSelector(state => state.tasks)
-    const isTasksLoading = tasks.status === 'loading'
+export default function TableTasks({ show }) {
+    const dispatch = useDispatch();
+    const { tasks } = useSelector(state => state.tasks);
+    const isTasksLoading = tasks.status === 'loading';
     
-    const [showCreateTaskForm, setShowCreateTaskForm] = useState(false)
-    const showCTF = () => {setShowCreateTaskForm(true)}
-    const hideCTF = () => {setShowCreateTaskForm(false)}
+    const [showCreateTaskForm, setShowCreateTaskForm] = useState(false);
+    const showCTF = () => { setShowCreateTaskForm(true) };
+    const hideCTF = () => { setShowCreateTaskForm(false) };
 
     useEffect(() => {
-        dispatch(fetchTasks({projectid: window.localStorage.getItem("projectid")}))
-    }, [showCreateTaskForm, show])
+        dispatch(fetchTasks({ projectid: window.localStorage.getItem("projectid") }));
+    }, [showCreateTaskForm, show]);
 
+    const settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
 
-    if(show) { 
-        return(
+    if (show) {
+        return (
             <>
-                <div className={styles.content}>
-                    <CreateTaskForm show={showCreateTaskForm} onClose={hideCTF}/>
-                    <div className={styles.tasksCurrent}>
-                    <div className={styles.tasksHeader}>
-                        <Typography variant='h5' className={styles.tiitle}>Текущие</Typography>
-                        <button onClick={showCTF} className={styles.button}>Добавить</button>
-                    </div>
-                        {tasks.items.map((obj, index) => 
-                        isTasksLoading || obj.iscomplete === true ? (
-                            <>
-                            </>
-                        ) : (
-                            <Task
-                                key={index}
-                                taskid={obj.taskid}
-                                taskname={obj.taskname}
-                                executor={obj.executor}
-                                iscomplet={obj.iscomplete}
-                            />
-                        ))}
-                    </div>
-                
-                    <div className={styles.tasksCompleted}>
+                <div className={styles.desktopView}>
+                    <CreateTaskForm show={showCreateTaskForm} onClose={hideCTF} />
+                    <div className={styles.tasksColumn}>
                         <div className={styles.tasksHeader}>
-                            <Typography variant='h5' className={styles.tiitle}>Выполненные</Typography>
+                            <Typography variant='h5' className={styles.title}>Текущие</Typography>
+                            <button onClick={showCTF} className={styles.button}>Добавить</button>
                         </div>
-                            {tasks.items.map((obj, index) => 
-                            isTasksLoading || obj.iscomplete === false ? (
-                                <>
-                                </>
-                            ) : (
+                        <div className={styles.tasksGrid}>
+                            {tasks.items.filter(task => !task.iscomplete).map((obj, index) => (
                                 <Task
                                     key={index}
                                     taskid={obj.taskid}
@@ -62,9 +121,59 @@ export default function TableTasks({show}) {
                                     iscomplet={obj.iscomplete}
                                 />
                             ))}
+                        </div>
+                    </div>
+                    <div className={styles.tasksColumn}>
+                        <div className={styles.tasksHeader}>
+                            <Typography variant='h5' className={styles.title}>Выполненные</Typography>
+                        </div>
+                        <div className={styles.tasksGrid}>
+                            {tasks.items.filter(task => task.iscomplete).map((obj, index) => (
+                                <Task
+                                    key={index}
+                                    taskid={obj.taskid}
+                                    taskname={obj.taskname}
+                                    executor={obj.executor}
+                                    iscomplet={obj.iscomplete}
+                                />
+                            ))}
+                        </div>
                     </div>
                 </div>
+                <div className={styles.mobileView}>
+                    <Slider {...settings}>
+                        <div className={styles.tasksCurrentMobile}>
+                            <div className={styles.tasksHeaderMobile}>
+                                <Typography variant='h5' className={styles.title}>Текущие</Typography>
+                                <button onClick={showCTF} className={styles.button}>Добавить</button>
+                            </div>
+                            {tasks.items.filter(task => !task.iscomplete).map((obj, index) => (
+                                <Task
+                                    key={index}
+                                    taskid={obj.taskid}
+                                    taskname={obj.taskname}
+                                    executor={obj.executor}
+                                    iscomplet={obj.iscomplete}
+                                />
+                            ))}
+                        </div>
+                        <div className={styles.tasksCompletedMobile}>
+                            <Typography variant='h5' className={styles.title}>Выполненные</Typography>
+                            {tasks.items.filter(task => task.iscomplete).map((obj, index) => (
+                                <Task
+                                    key={index}
+                                    taskid={obj.taskid}
+                                    taskname={obj.taskname}
+                                    executor={obj.executor}
+                                    iscomplet={obj.iscomplete}
+                                />
+                            ))}
+                        </div>
+                    </Slider>
+                </div>
             </>
-        ) 
+        );
     }
+
+    return null;
 }
